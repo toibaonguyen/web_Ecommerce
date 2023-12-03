3a850968e511ee14f51d982b4b62cd1dd871ae85
QuanPhan <phanhoangminhquan240402@gmail.com>
2023-12-03 20:33:46 +0700

    Login-Signup Form

---
+++ src/pages/account/LoginsignupComponent/Header.js
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+
+export default function Header({
+    heading,
+    paragraph,
+    linkName,
+    linkUrl="Login"
+}){
+    return(
+        <div className="mb-10">
+            <div className="flex justify-center text-white">
+                <img 
+                    alt=""
+                    className="h-20 w-30"
+                    src="https://upload.wikimedia.org/wikipedia/commons/6/64/Logo_Tiki.png"/>
+            </div>
+            <h2 className="mt-6 text-center text-5xl font-extrabold text-gray-900 text-#D6E6F2">
+                {heading}
+            </h2>
+            <p className="mt-2 text-center text-2xl text-gray-600 mt-5">
+            {paragraph} {' '}
+            <Link href={linkUrl} className="font-medium text-purple-600 hover:text-purple-500">
+                {linkName}
+            </Link>
+            </p>
+        </div>
+    )
+}
